09d5237148e8055b90197745ea562f9b0905ac1e
DochiTutoring <18k7102dy@naver.com>
2023-10-07 20:55:14 +0900

    helloJob을 commandLineRunner로 실행하는 코드 추가

---
+++ chapter1/src/main/kotlin/me/example/chapter1/config/HelloConfiguration.kt
@@ -0,0 +1,57 @@
+package me.example.chapter1.config
+
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.batch.core.launch.support.RunIdIncrementer
+import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.step.builder.StepBuilder
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+
+/**
+ * @author Doyeop Kim
+ * @since 2023/10/07
+ */
+@Configuration
+class HelloConfiguration(
+    private val jobRepository: JobRepository,
+    private val transactionManager: PlatformTransactionManager,
+    private val tasklet: HelloTasklet,
+    private val jobLauncher: JobLauncher
+) {
+    companion object {
+        const val JOB_NAME = "helloJob"
+        const val JOB_STEP = "${JOB_NAME}_STEP"
+    }
+
+    @Bean
+    fun jobStartUp(): CommandLineRunner {
+        return CommandLineRunner {
+            val jobParameters = JobParametersBuilder()
+                .addLong("time", System.currentTimeMillis())
+                .toJobParameters()
+
+            jobLauncher.run(job(), jobParameters)
+        }
+    }
+
+    @Bean
+    fun job(): Job {
+        return JobBuilder(JOB_NAME, jobRepository)
+            .incrementer(RunIdIncrementer())
+            .start(step())
+            .build()
+    }
+
+    @Bean
+    fun step(): Step {
+        return StepBuilder(JOB_STEP, jobRepository)
+            .tasklet(tasklet, transactionManager)
+            .build()
+    }
+}
---
+++ chapter1/src/main/kotlin/me/example/chapter1/config/HelloTasklet.kt
@@ -0,0 +1,23 @@
+package me.example.chapter1.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.stereotype.Component
+
+/**
+ * @author Doyeop Kim
+ * @since 2023/10/07
+ */
+@Component
+@StepScope
+class HelloTasklet: Tasklet {
+    override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus? {
+        println("Hello!")
+
+        return RepeatStatus.FINISHED
+    }
+}
